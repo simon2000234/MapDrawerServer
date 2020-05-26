62ca31554dea65832bd366e5ae0578942a7230db
Simon <44614995+simon2000234@users.noreply.github.com>
2020-05-26 10:19:04 +0200

    map maker server

---
+++ src/MapMaker.java
@@ -0,0 +1,49 @@
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JComponent;
+
+public class MapMaker extends JComponent{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private List<Point> points = new ArrayList<>();
+	
+	@Override
+	protected void paintComponent(Graphics g)
+	{
+		super.paintComponents(g);
+		
+		Graphics twoDGraphics = (Graphics2D) g;
+		
+		int centerX = getWidth() / 2;
+		int centerY = getHeight() / 2;
+		
+		if(!points.isEmpty())
+		{
+			Point lastPoint = points.get(0);
+			
+			for (int i = 1; i < points.size(); i++) {
+				Point newPoint = points.get(i);
+				
+				if(lastPoint.distance(newPoint) < 40)
+				{
+					twoDGraphics.drawLine(lastPoint.x + centerX, centerY - lastPoint.y, newPoint.x + centerX, centerY - newPoint.y);
+				}
+				lastPoint = newPoint;
+			}
+		}
+	}
+	
+	public void addPosition(double x, double y)
+	{
+		points.add(new Point((int)x, (int)y));
+		repaint();
+	}
+}
--- src/PcServer.java
+++ src/PcServer.java
@@ -4,9 +4,22 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+import javax.swing.JFrame;
+
 public class PcServer {
 
 	public static void main(String[] args) {
+		System.out.println("Server Started");
+		JFrame frame = new JFrame();
+		
+		frame.setTitle("Dab on the Map");
+		frame.setSize(1000, 1000);
+		frame.setLocationRelativeTo(null);
+		
+		MapMaker mk = new MapMaker();
+		frame.add(mk);
+		frame.setVisible(true);
+		
 		try
 		(
 				ServerSocket ss = new ServerSocket(5000);
@@ -21,7 +34,13 @@ public class PcServer {
 			{
 				String message = dis.readUTF();
 				System.out.println("Client says: " + message);
-				
+				if(message.contains(","))
+				{
+					String[] sArray = message.split(",");
+					double x = Double.parseDouble(sArray[0]);
+					double y = Double.parseDouble(sArray[1]);
+					mk.addPosition(x, y);
+				}
 				if(message.equalsIgnoreCase("quit"))
 				{
 					done = true;
@@ -29,7 +48,6 @@ public class PcServer {
 			}
 			
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
